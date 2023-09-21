29f0718af229b1a6936bbc987cdad007e6ea5799
Robert Gleason <l2ob122@gmail.com>
2023-09-20 21:42:48 -0700

    added logic inside the products  service

--- frontend/angular-eccomerce/src/app/services/product.service.ts
+++ frontend/angular-eccomerce/src/app/services/product.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { Product } from '../common/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  private baseUrl = 'http://localhost:8080/api/products';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getProductList(): Observable<Product[]> {
+    // maps the json data from spring data rest to product array
+    return this.httpClient
+      .get<GetResponse>(this.baseUrl)
+      .pipe(map((response) => response._embedded.products));
+  }
+}
+
+interface GetResponse {
+  // unwraps the json from spring data rest _embedded entry
+  _embedded: {
+    products: Product[];
+  };
 }
