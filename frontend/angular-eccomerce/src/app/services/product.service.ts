38847c44fdb8959dd69baeef7d259ba40c8665ee
Robert Gleason <l2ob122@gmail.com>
2023-09-21 18:35:49 -0700

    created the getProductCategories() and connected to the rest

--- frontend/angular-eccomerce/src/app/services/product.service.ts
+++ frontend/angular-eccomerce/src/app/services/product.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
   private baseUrl = 'http://localhost:8080/api/products';
+  private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) {}
 
@@ -17,16 +19,26 @@ export class ProductService {
 
     // maps the json data from spring data rest to product array
     return this.httpClient
-      .get<GetResponse>(searchUrl)
+      .get<GetResponseProducts>(searchUrl)
       .pipe(map((response) => response._embedded.products));
   }
 
-  getProductCategories() {}
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient
+      .get<GetResponseProductCategory>(this.categoryUrl)
+      .pipe(map((response) => response._embedded.productCategory));
+  }
 }
 
-interface GetResponse {
-  // unwraps the json from spring data rest _embedded entry
+interface GetResponseProducts {
+  // unwraps the JSON from spring data rest _embedded entry
   _embedded: {
     products: Product[];
+  };
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   };
 }
